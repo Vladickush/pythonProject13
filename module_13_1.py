4d61ab6c360316ec559d866270398fe847664e4b
Vladickush <u.vn@mail.ru>
2024-11-16 22:19:29 +0300

    Add files via upload

---
+++ module_13_1.py
@@ -0,0 +1,28 @@
+# Тема "Асинхронность на практике"
+# Задача "Асинхронные силачи":
+import time
+import asyncio
+
+
+async def start_strongman(name, power):
+    print(f'Силач {name} начал соревнования.')
+    balls = 'шар'
+    for i in range(1, 6):
+        await asyncio.sleep(1 / power)
+        if i > 4:   balls = 'шаров'
+        elif i > 1: balls = 'шара'
+        print(f'Силач {name} поднял {i} {balls}')
+    print(f'Силач {name} закончил соревнования.')
+
+
+async def start_tournament():
+    man1 = asyncio.create_task(start_strongman('Pasha', 3))
+    man2 = asyncio.create_task(start_strongman('Denis', 4))
+    man3 = asyncio.create_task(start_strongman('Apollon', 5))
+
+    await man1
+    await man2
+    await man3
+
+
+asyncio.run(start_tournament())
