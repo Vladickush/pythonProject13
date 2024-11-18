b96d4d78f0362ef9a08b7b86b31318fe6ba1c8ed
Vladickush <u.vn@mail.ru>
2024-11-18 23:27:37 +0300

    Add files via upload

---
+++ module_13_2.py
@@ -0,0 +1,21 @@
+# Тема "Хендлеры обработки сообщений".
+# Задача "Бот поддержки (Начало)":
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api = "7487519575:AAEal-5CylTeImVLBt1zXEWDpfHEuNg1MG4"
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage= MemoryStorage())
+
+@dp.message_handler(commands=["start"])
+async def start_message(message):
+    print("Привет! Я бот помогающий твоему здоровью.")
+
+@dp.message_handler()
+async def all_message(message):
+    print("Введите команду /start, чтобы начать общение.")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
